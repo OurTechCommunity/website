62ba2702d5cd6c56c5d402cc3561d09cca21c5cf
Auto deploy GitHub Pages <cd-deploy-gh-pages@bot>
2021-12-14 14:41:52 +0000

    Deploy to pages: 2021-12-14 14:41:52

--- static/js/script.js
+++ static/js/script.js
@@ -1,12 +1,32 @@
 const ul = document.querySelector(".navbar-ul");
 const burgerMenu = document.querySelector(".burger-menu");
+const title = document.querySelector("h1");
+const teamContainer = document.querySelector(".team-container");
 
 // For burger menu
-burgerMenu.addEventListener("click", (event) => {
-	ul.classList.toggle("hide-burger-menu");
-	event.stopPropagation();
-});
+burgerMenu.addEventListener(
+	"click",
+	(event) => {
+		ul.classList.toggle("hide-burger-menu");
+		event.stopPropagation();
+	},
+	{ passive: true }
+);
 
-window.addEventListener("click", () => {
-	ul.classList.add("hide-burger-menu");
-});
+window.addEventListener(
+	"click",
+	() => {
+		ul.classList.add("hide-burger-menu");
+	},
+	{ passive: true }
+);
+
+// For team page
+if (window.location.pathname.startsWith("/team"))
+	title.addEventListener(
+		"click",
+		() => {
+			teamContainer.classList.toggle("cto-visible");
+		},
+		{ passive: true }
+	);
